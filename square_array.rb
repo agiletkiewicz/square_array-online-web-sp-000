fe3e1a7f75a98043d0e571479b0ec8c2c5fdc27e
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-04-22 00:51:48 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,7 @@
 def square_array(array)
   index = 0
   array.each do |number|
-    number ** number = squared
-    array[index] = squared
+    array[index] = number * number
     index += 1
   end
 end
